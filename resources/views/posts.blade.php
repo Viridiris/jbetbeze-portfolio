f6ae3e58af5cd98794dab99ec262538cbd72e906
Viridiris <josephbetbeze@gmail.com>
2021-09-15 21:11:15 -0400

    Added default laravel project files

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<title>My Blog</title>
+
+<link rel="stylesheet" href="/app.css">
+
+
+<body>
+<article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit ametc, consectetur adipiscing elit. Morbi viverra vehicula. Who gave you permission to laugh.
+    </p>
+</article>
+<article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit ametc, consectetur adipiscing elit. Morbi viverra vehicula
+    </p>
+</article>
+<article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit ametc, consectetur adipiscing elit. Morbi viverra vehicula
+    </p>
+</article>
+
+
+</body>
