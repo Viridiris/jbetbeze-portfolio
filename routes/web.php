145bfea7aaa80ac749ef7800c30bd659d8677e93
Viridiris <josephbetbeze@gmail.com>
2021-09-16 00:36:08 -0400

    Cleaned up the code on the route web.php file

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,8 @@ Route::get('/', function () {
 
 Route::get('posts/{post}', function ($slug) {
 
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if(! file_exists($path)) {
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         return redirect('/');
     }
 
@@ -34,7 +33,5 @@ Route::get('posts/{post}', function ($slug) {
 
     $post = file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html");
 
-    return view('post', [
-        'post' => $post
-    ]);
+    return view('post', ['post' => $post]);
 })->where('post', '[A-z_\-]+');
